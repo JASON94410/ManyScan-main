f91e8483d405306abe67fb57ea60910f7be9f0aa
JASON <jason.seban.pati@hotmail.fr>
2024-04-08 15:18:13 +0200

    test ssss

--- server/routes/mangas.routes.js
+++ server/routes/mangas.routes.js
@@ -5,6 +5,6 @@ router.get("/favorites", mangasControllers.getFavoriteMangas);
 router.get("/search", mangasControllers.oneMangaByTitle);
 router.get("/", mangasControllers.allMangas);
 router.post("/like-manga", mangasControllers.addFavoriteMangas);
-router.delete("/unlike-manga", mangasControllers.deleteFavoriteMangas);
+router.delete("/unlike-mangasss", mangasControllers.deleteFavoriteMangas);
 
 module.exports = router;
